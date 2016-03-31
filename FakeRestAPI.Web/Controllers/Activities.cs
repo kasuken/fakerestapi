97d3528a1844bd697b9b7f814f16eacf00eee316
Emanuele B <bartolesiemanuele@hotmail.com>
2016-03-31 02:07:40 +0200

    Activity Model and Controller

---
+++ FakeRestAPI.Web/Controllers/Activities.cs
@@ -0,0 +1,65 @@
+﻿using FakeRestAPI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FakeRestAPI.Web.Controllers
+{
+    public class Activities : ApiController
+    {
+        List<Activity> _Activities = new List<Activity>();
+
+        public Activities()
+        {
+            for (int i = 1; i < 31; i++)
+            {
+                var activity = new Activity();
+                activity.ID = i;
+                activity.Title = string.Format("Activity {0}", i.ToString());
+                activity.DueDate = DateTime.Now.AddHours(i);
+
+                _Activities.Add(activity);
+            }
+        }
+
+        // GET api/<controller>
+        public IEnumerable<Activity> Get()
+        {
+            return _Activities;
+        }
+
+        // GET api/<controller>/5
+        public IHttpActionResult Get(int id)
+        {
+            var activity =_Activities.Where(b => b.ID == id).FirstOrDefault();
+
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(activity);
+        }
+
+        // POST api/<controller>
+        public IHttpActionResult Post([FromBody]Activity activity)
+        {
+            return Ok(activity);
+        }
+
+        // PUT api/<controller>/5
+        public IHttpActionResult Put(int id, [FromBody]Activity activity)
+        {
+            return Ok(activity);
+        }
+
+        // DELETE api/<controller>/5
+        public IHttpActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
